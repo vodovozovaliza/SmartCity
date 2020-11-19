28a7ee8d2314159e071386f100cc7396c0872702
unknown <allin.semen@gmail.com>
2020-11-19 12:58:26 +0200

    Optimization and add comments

--- view.py
+++ view.py
@@ -7,38 +7,31 @@ dpi = 80
 def show(input_filename, weights):
 
     df = pd.read_csv(input_filename)
-    columns = []
-    for i in df:
-        columns.append(i)
-    n = len(columns)
 
-    #data = [['Kharkiv', 8652], ['Kiev', 9124], ['Odessa', 7592]]
-    data = []
-    for i in range(len(df[columns[0]])):
-        kpi = 0
-        for j in range(1, n):
-            kpi += weights[j-1] * df[columns[j]][i]
-        data.append([df[columns[0]][i], kpi])
+    # data processing
+    df['Total score'] = (df[df.columns[1:]] * weights).sum(axis=1)
+    # sort values by total score
+    df.sort_values(by='Total score', ascending=True, inplace=True)
 
-    data = sorted(data, key=lambda a: a[1])
-
-    data_names = []
-    data_values = []
-    for i in data:
-        data_names.append(i[0])
-        data_values.append(i[1])
-
+    # Set font size
     mpl.rcParams.update({'font.size': 9})
+    # Create figure
     _, ax = plt.subplots(dpi = dpi, figsize = (512 / dpi, 384 / dpi), num = 'Test')
+    # Drawing vertical lines for x axis
     ax.xaxis.grid(True, zorder = 1)
     ax.set_title('Test')
-    xs = range(len(data_values))
+    # The y coordinates of the bars
+    xs = range(len(df['Total score'].values))
 
-    plt.barh([x for x in xs], data_values,
+    # Create a horizontal bar plots
+    plt.barh(xs, df['Total score'].values,
             height = 0.2, color = 'blue', alpha = 0.7, label = 'KPI',
             zorder = 2)
-    plt.yticks(xs, data_names, rotation = 10)
 
+    # Draw names of objects
+    plt.yticks(xs, df[df.columns[0]].values, rotation = 10)
+
+    # Set legend location
     plt.legend(loc='upper right')
     plt.savefig('result.png')
     plt.show()
