318175a4c9b2b255dd7394ec9354c8beb7819fe8
unknown <allin.semen@gmail.com>
2020-11-20 13:48:46 +0200

    Add normalize

--- view.py
+++ view.py
@@ -1,15 +1,35 @@
+import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 dpi = 80
 
+def normalize(df):
+    result = df.copy()
+    for feature_name in df.columns[1:]:
+        # Mean
+        mean = df[feature_name].mean()
+        # Count of values
+        n = len(df[feature_name])
+        # Sample standart deviation
+        ssd = np.sqrt((1 / (n - 1)) * np.power(df[feature_name] - mean, 2).sum())
+
+        result[feature_name] = (df[feature_name] - mean) / ssd
+    return result
+
 def show(input_filename, weights):
 
     df = pd.read_csv(input_filename)
 
+    # Normalize
+    #print(df[df.columns[1:]])
+    df = normalize(df)
+    #print(df[df.columns[1:]])
+
     # data processing
     df['Total score'] = (df[df.columns[1:]] * weights).sum(axis=1)
+    #print(df['Total score'])
     # sort values by total score
     df.sort_values(by='Total score', ascending=True, inplace=True)
 
@@ -37,5 +57,5 @@ def show(input_filename, weights):
     plt.show()
 
 if __name__ == '__main__':
-    #show('movehubcostofliving.csv', [0.1, 0.1, 0.1, 0.2, 0.2, 0.4])
-    show('test.csv', [0.1, 0.1, 0.1, 0.2, 0.2, 0.4])
+    show('movehubcostofliving.csv', [0.1, 0.1, 0.1, 0.2, 0.2, 0.4])
+    #show('test.csv', [0.1, 0.1, 0.1, 0.2, 0.2, 0.4])
