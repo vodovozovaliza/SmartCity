56e68cb16f383c1204d641e2a3f49fa56e6e74ec
Johnny-Bang <74344540+Johnny-Bang@users.noreply.github.com>
2020-11-22 23:53:07 +0200

    Add files via upload

---
+++ mai.py
@@ -0,0 +1,42 @@
+import numpy as np
+def coherence(arr, w):
+    #calculate maximum eigenvalue of matrix
+    n = arr[0].size
+    p = arr @ w
+    p /= w
+    lam = p.sum()
+    lam /= n
+    #calculate concordance factor
+    ci = (lam - n) / (n - 1)
+    #calculate random index
+    ri = 1.98 * (n - 2) / n
+    #calculate coherence
+    cr = ci / ri
+    return cr
+
+def MAI(arr):
+    n = arr[0].size
+    init = np.copy(arr)
+    #normalize the matrix
+    for i in range(n):
+        s = 0
+        for j in range(n):
+            s += arr[j][i]
+        for j in range(n):
+            arr[j][i] /= s
+    #calculate the relative weights of indicators
+    res = np.arange(n, dtype=float)
+    for i in range(n):
+        res[i] = arr[i].sum() / n
+    #check if matrix coherence is satisfying
+    if coherence(init, res) > 0.1:
+        return [-1]
+    return res
+
+if __name__ == '__main__':
+    #initialize data
+    a = np.array([[1, 2, 1/3, 1/5, 1/7], [1/2, 1, 1/6, 1/9, 1/9], [3, 6, 1, 1/2, 1/2], [5, 9, 2, 1, 1/2], [7, 9, 2, 2, 1]])
+    #print initial data
+    print(a)
+    #print resulting data
+    print(MAI(a))
